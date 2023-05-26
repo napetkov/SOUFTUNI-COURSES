099dcb3972c05c27e81fc7ed708a9bb62ce77a8f
Nikolay Petkov <106302204+napetkov@users.noreply.github.com>
2023-05-26 22:35:38 +0300

    Update Table-Relation-Exercises.sql
    Task 3

--- MySQL_May_2023/5.Table-Relations/Table-Relation-Exercises.sql
+++ MySQL_May_2023/5.Table-Relations/Table-Relation-Exercises.sql
@@ -65,11 +65,60 @@ VALUES ('BMW', '1916/03/01'),
        ('Tesla', '2003/01/01'),
        ('Lada', '1966/05/01');
 
-insert into models (model_id,name, manufacturer_id)
-values (101,'X1', 1),
+insert into models (model_id, name, manufacturer_id)
+values (101, 'X1', 1),
        (102, 'i6', 1),
        (103, 'Model S', 2),
        (104, 'Model X', 2),
        (105, 'Model 3', 2),
        (106, 'Nova', 3);
+
+# 3
+create table students
+(
+    student_id int auto_increment not null primary key,
+    name       varchar(30)
+);
+
+create table exams
+(
+    exam_id int auto_increment not null primary key,
+    name    varchar(30)
+);
+
+create table students_exams
+(
+    student_id int not null,
+    exam_id    int not null,
+    constraint pk__students_student_id__exams_exams_id
+        primary key (student_id, exam_id)
+);
+
+alter table students_exams
+    add constraint fk__students_exams_student_id__students_students_id
+        foreign key (student_id)
+            references students (student_id);
+
+alter table students_exams
+    add constraint fk__students_exams_exam_id__exams_exam_id
+        foreign key (exam_id)
+            references exams (exam_id);
+
+insert into students(name)
+VALUES ('Mila'),
+       ('Toni'),
+       ('Ron');
+
+insert into exams (exam_id, name)
+VALUES (101, 'Spring MVC'),
+       (102, 'Neo4j'),
+       (103, 'Oracle 11g');
+
+insert into students_exams (student_id, exam_id)
+VALUES (1, 101),
+       (1, 102),
+       (2, 101),
+       (3, 103),
+       (2, 102),
+       (2, 103);
 
