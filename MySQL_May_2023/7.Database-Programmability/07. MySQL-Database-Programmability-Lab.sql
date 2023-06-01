5d36a58137c5549f0a906542465ff7651cba684a
Nikolay Petkov <106302204+napetkov@users.noreply.github.com>
2023-06-01 23:10:26 +0300

    Update 07. MySQL-Database-Programmability-Lab.sql
    Task 2 and many examples

--- MySQL_May_2023/7.Database-Programmability/07. MySQL-Database-Programmability-Lab.sql
+++ MySQL_May_2023/7.Database-Programmability/07. MySQL-Database-Programmability-Lab.sql
@@ -9,22 +9,90 @@ delimiter ;
 ;
 
 
-
+# 1 Functions
 delimiter $$
-create function count_employees_by_town(`town_name` varchar(50))
+create function ufn_count_employees_by_town(town_name varchar(50))
     returns int
     deterministic
 begin
-    declare e_count int;
-    set e_count := (select count(*)
+    return (select count(*)
             from employees as e
                      join addresses as a on e.address_id = a.address_id
-                     join towns t on a.town_id = t.town_id
+                     join towns as t on a.town_id = t.town_id
             where t.name = town_name);
-    return e_count;
 end $$
 
 delimiter ;
 ;
-select count_employees_by_town('Sofia') as count;
-select name, count_employees_by_town(name) as count from towns;
+select ufn_count_employees_by_town('Sofia') as count;
+select name, ufn_count_employees_by_town(name) as count
+from towns;
+
+# 2 Procedures
+delimiter $$
+create procedure usp_select_employees()
+begin
+    select * from employees;
+end $$
+delimiter ;
+;
+call usp_select_employees();
+
+# Procedures with parameters
+delimiter $$
+create procedure usp_select_employees(max_id int)
+begin
+    select * from employees where employee_id < max_id;
+end $$
+delimiter ;
+;
+call usp_select_employees(35);
+
+delimiter $$
+create procedure usp_raise_salaries(percent decimal(3, 2), out total_increase DECIMAL(19, 4))
+begin
+    declare local_increase decimal(19, 2);
+    set local_increase := (select sum(salary) * (1 + percent) - sum(salary) as total_sum from employees);
+    set total_increase = local_increase;
+    update employees set salary = salary * (1 + percent);
+
+end $$
+delimiter ;
+;
+
+set @increase = 0;
+
+call usp_raise_salaries(5, @increase);
+
+select @increase;
+
+#  lab.2 - parameter percent
+delimiter $$
+create procedure usp_raise_salaries_by_diven_percent(percent decimal(3, 2))
+begin
+    declare actual_percent decimal(3, 2);
+    set actual_percent = 1 + percent / 100;
+    update employees set salary = salary * actual_percent;
+end $$
+delimiter ;
+;
+
+call usp_raise_salaries_by_diven_percent(5);
+
+select *
+from employees
+where department_id = 10
+order by first_name, salary;
+
+
+# Lab 2 - parameter department
+delimiter $$
+create procedure usp_raise_salaries(department_name varchar(50))
+begin
+    update employees as e
+    join departments as d on d.department_id = e.department_id
+    set salary = salary * 1.05
+    where d.name = department_name;
+end $$
+delimiter ;
+;
