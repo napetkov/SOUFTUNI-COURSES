083a6dda1571f57ed7d2f05d7660c38459a9e2c1
Nikolay Petkov <106302204+napetkov@users.noreply.github.com>
2023-06-04 16:29:19 +0300

    Update 2023-06-02_Exam_prep.sql
    all set of Tasks

--- MySQL_May_2023/8.Exam-prep/2023-06-02_Exam-prep/2023-06-02_Exam_prep.sql
+++ MySQL_May_2023/8.Exam-prep/2023-06-02_Exam-prep/2023-06-02_Exam_prep.sql
@@ -80,10 +80,9 @@ create table orders_products
 
 # 2
 insert into products(name, type, price)
-select
-    concat_ws(' ', last_name , 'specialty' ) as name,
-    'Cocktail' as type,
-    ceiling(salary * 0.01) as price
+select concat_ws(' ', last_name, 'specialty') as name,
+       'Cocktail'                             as type,
+       ceiling(salary * 0.01)                 as price
 from waiters
 where id > 6;
 
@@ -93,19 +92,95 @@ set table_id = table_id - 1
 where id between 12 and 23;
 
 # 4
-delete w from waiters as w
-left join orders o on w.id = o.waiter_id
+delete w
+from waiters as w
+         left join orders o on w.id = o.waiter_id
 where o.id is null;
 
 # 5
-select * from clients
-order by birthdate desc , id desc;
+select *
+from clients
+order by birthdate desc, id desc;
 
 # 6
-select first_name, last_name, birthdate, review from clients as c
+select first_name, last_name, birthdate, review
+from clients as c
 where c.card is null
-and year(birthdate) between 1978 and 1993
-order by last_name desc , id asc
+  and year(birthdate) between 1978 and 1993
+order by last_name desc, id asc
 limit 5;
 
 # 7
+alter table waiters
+    drop username,
+    drop password;
+# insert into waiters(username,password)
+# values;
+select concat(
+               last_name,
+               first_name,
+               length(first_name),
+               'Restaurant')              as username,
+       reverse(substring(w.email, 2, 12)) as password
+from waiters as w
+where w.salary is not null
+order by password desc;
+
+# 8
+select p.id, p.name, count(p.id) as count
+from orders as o
+         join orders_products as op on o.id = op.order_id
+         join products p on op.product_id = p.id
+group by p.name
+having count >= 5
+order by count desc, p.name asc;
+
+# 9
+show variables like 'sql_mode'
+set sql_mode = 'STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+select id,
+       capacity,
+       count_clients,
+       (case
+            when count_clients > capacity then 'Extra seats'
+            when count_clients = capacity then 'Full'
+            when count_clients < capacity then 'Free seats'
+           end)
+           as availability
+from (select t.id, t.capacity, count(oc.client_id) as count_clients
+      from tables as t
+               join orders o on t.id = o.table_id
+               join orders_clients oc on o.id = oc.order_id
+      where t.floor = 1
+      group by (t.id)) as count_of_sit_people
+order by id desc;
+
+# 10
+delimiter $$
+create function udf_client_bill(full_name VARCHAR(50))
+    returns decimal(10, 2)
+    deterministic
+begin
+    return (select sum(price)
+            from clients as c
+                     join orders_clients as oc on c.id = oc.client_id
+                     join orders_products as op on oc.order_id = op.order_id
+                     join products as p on p.id = op.product_id
+            where concat_ws(' ', c.first_name, c.last_name) = full_name);
+end $$
+delimiter ;
+
+
+# 11
+delimiter $$
+create procedure udp_happy_hour (type varchar(50))
+begin
+update products as p
+    set price = price * 0.80
+    where p.type = type and price >= 10;
+end $$
+delimiter ;
+
+select * from products as p
+where p.type = 'Cognac';
