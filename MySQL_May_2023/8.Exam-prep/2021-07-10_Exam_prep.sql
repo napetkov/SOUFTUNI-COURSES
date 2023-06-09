62a11766727881788b110e59b55d8e6a5f3b9f5b
Nikolay Petkov <106302204+napetkov@users.noreply.github.com>
2023-06-10 00:26:28 +0300

    Update 2021-07-10_Exam_prep.sql
    task 8 to 11

--- MySQL_May_2023/8.Exam-prep/2021-07-10_Exam_prep.sql
+++ MySQL_May_2023/8.Exam-prep/2021-07-10_Exam_prep.sql
@@ -1,3 +1,6 @@
+show variables like 'sql_mode';
+set sql_mode = 'STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
 # 1
 create table addresses
 (
@@ -107,14 +110,89 @@ where c.mileage is not null
 order by mileage desc, first_name;
 
 # 7
-select
-    c.id as car_id,
-    c.make as make,
-    c.mileage as mieage,
-    count(c2.id) as count_of_courses,
-    round(avg(c2.bill), 2) as avg_bill
+select c.id                   as car_id,
+       c.make                 as make,
+       c.mileage              as mieage,
+       count(c2.id)           as count_of_courses,
+       round(avg(c2.bill), 2) as avg_bill
 from cars as c
-         join courses c2 on c.id = c2.car_id
+         left join courses c2 on c.id = c2.car_id
 group by c.id
-having count_of_courses > 2
-order by count_of_courses desc ,c.id;
+having count_of_courses != 2
+order by count_of_courses desc, c.id;
+
+# 8
+select c.full_name,
+       count(c3.id) as count_of_carts,
+       sum(c2.bill) as total_sum
+from clients as c
+         join courses c2 on c.id = c2.client_id
+         join cars c3 on c3.id = c2.car_id
+where substring(full_name, 2, 1) = 'a'
+group by c.full_name
+having COUNT_OF_CARTS > 1
+order by full_name;
+
+# 9
+select a.name,
+       case
+           when hour(c.start) between 6 and 20 then 'Day'
+           when hour(c.start) < 6 or hour(c.start) > 20 then 'Night'
+           end as day_time,
+       c.bill,
+       c2.full_name,
+       c3.make,
+       c3.model,
+       c4.name as category_name
+from courses as c
+         join addresses a on a.id = c.from_address_id
+         join clients c2 on c.client_id = c2.id
+         join cars c3 on c3.id = c.car_id
+         join categories c4 on c4.id = c3.category_id
+order by c.id;
+
+# 10
+delimiter //
+create function udf_courses_by_client(phone_num VARCHAR(20))
+    returns int
+    deterministic
+begin
+    return (select count(c.id)
+            from courses as c
+                     join clients c2 on c2.id = c.client_id
+            where c2.phone_number = phone_num
+            group by (c2.id));
+end //
+delimiter ;
+
+select count(c.id)
+from courses as c
+         join clients c2 on c2.id = c.client_id
+where c2.phone_number = '(803) 6386812'
+group by (c2.id);
+
+# 11
+delimiter //
+create procedure udp_courses_by_address(`address_name` varchar(100))
+begin
+    select a.name   as name,
+           cl.full_name,
+           case
+               when c.bill <= 20 then 'Low'
+               when c.bill > 20 and c.bill <= 30 then 'Medium'
+               else 'High'
+               end
+                    as level_of_bill,
+           cars.make,
+           cars.`condition`,
+           cat.name as cat_name
+    from addresses as a
+             join courses c on a.id = c.from_address_id
+             join clients cl on c.client_id = cl.id
+             join cars on cars.id = c.car_id
+             join categories cat on cars.category_id = cat.id
+    where a.name = `address_name`
+    order by cars.make, cl.full_name;
+end //
+delimiter ;
+
