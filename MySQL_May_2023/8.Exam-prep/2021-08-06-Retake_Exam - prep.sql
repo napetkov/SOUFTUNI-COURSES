c6fb1ebd63dc185c19af92319fcdb1bfb1649274
Nikolay Petkov <106302204+napetkov@users.noreply.github.com>
2023-06-09 06:30:05 +0300

    Update 2021-08-06-Retake_Exam - prep.sql
    tasks from 4 to 7

--- MySQL_May_2023/8.Exam-prep/2021-08-06-Retake_Exam - prep.sql
+++ MySQL_May_2023/8.Exam-prep/2021-08-06-Retake_Exam - prep.sql
@@ -77,11 +77,10 @@ create table games_categories
 
 # 2
 insert into games(name, rating, budget, team_id)
-select
-    lower(reverse(substring(t.name, 2))),
-    t.id,
-    t.leader_id * 1000,
-    t.id
+select lower(reverse(substring(t.name, 2))),
+       t.id,
+       t.leader_id * 1000,
+       t.id
 from teams as t
 where t.id between 1 and 9;
 
@@ -89,6 +88,41 @@ where t.id between 1 and 9;
 update employees as e
     join teams t on e.id = t.leader_id
 set e.salary = e.salary + 1000
-where t.leader_id = e.id and age < 40 and salary <= 5000;
+where t.leader_id = e.id
+  and age < 40
+  and salary <= 5000;
 
 # 4
+delete g
+from games as g
+         left join games_categories as gc on g.id = gc.game_id
+where g.release_date is null
+  and gc.category_id is null;
+
+# 5
+select first_name, last_name, age, salary, happiness_level
+from employees
+order by salary, id;
+
+# 6
+select t.name              as team_name,
+       a.name              as address_name,
+       char_length(a.name) as count_of_characters
+from teams as t
+         left join offices o on t.office_id = o.id
+         left join addresses a on a.id = o.address_id
+where o.website is not null
+order by t.name, a.name;
+
+# 7
+select
+    c.name as name,
+    count(g.id) as games_count,
+    round(avg(g.budget),2) as avg_budget,
+    max(g.rating) as max_rating
+from categories as c
+         join games_categories as gc on c.id = gc.category_id
+         join games as g on gc.game_id = g.id
+group by (c.name)
+having max_rating >= 9.5
+order by games_count desc, name ;
