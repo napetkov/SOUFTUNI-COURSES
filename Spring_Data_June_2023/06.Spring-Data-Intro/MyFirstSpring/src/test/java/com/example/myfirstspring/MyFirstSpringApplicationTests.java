74415241b4e6df26caf3768203397ae2e5df1fdc
Nikolay Petkov <106302204+napetkov@users.noreply.github.com>
2023-07-03 17:40:49 +0300

    Spring Data Intro - Lab
    My First Spring app

---
+++ Spring_Data_June_2023/06.Spring-Data-Intro/MyFirstSpring/src/test/java/com/example/myfirstspring/MyFirstSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myfirstspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
