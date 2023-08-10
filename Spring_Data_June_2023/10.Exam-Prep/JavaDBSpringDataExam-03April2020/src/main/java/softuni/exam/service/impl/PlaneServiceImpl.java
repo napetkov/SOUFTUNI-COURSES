8863644352f22f698765f787e54dd411086cc237
Nikolay Petkov <106302204+napetkov@users.noreply.github.com>
2023-08-10 07:07:32 +0300

    Spring Data Exam prep Exam 03 April 2020
    Set up project

---
+++ Spring_Data_June_2023/10.Exam-Prep/JavaDBSpringDataExam-03April2020/src/main/java/softuni/exam/service/impl/PlaneServiceImpl.java
@@ -0,0 +1,20 @@
+package softuni.exam.service.impl;
+
+import org.springframework.stereotype.Service;
+import softuni.exam.service.PlaneService;
+public class PlaneServiceImpl implements PlaneService {
+    @Override
+    public boolean areImported() {
+        return false;
+    }
+
+    @Override
+    public String readPlanesFileContent() {
+        return null;
+    }
+
+    @Override
+    public String importPlanes() {
+        return null;
+    }
+}
