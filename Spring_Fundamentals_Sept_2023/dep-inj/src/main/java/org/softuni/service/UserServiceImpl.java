27c7559473f5527745423e93afc8040ad33aba11
Nikolay Petkov <106302204+napetkov@users.noreply.github.com>
2023-09-22 11:19:25 +0300

    Demo project for Dependency Injection

--- Spring_Fundamentals_Sept_2023/dep-inj/src/main/java/org/softuni/service/UserServiceImpl.java
+++ Spring_Fundamentals_Sept_2023/dep-inj/src/main/java/org/softuni/service/UserServiceImpl.java
@@ -10,6 +10,7 @@ import java.util.Optional;
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
 
+
     public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
