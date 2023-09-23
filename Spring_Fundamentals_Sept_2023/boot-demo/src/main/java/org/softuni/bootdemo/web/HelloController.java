e002ad18ae84fec4e17111946e4fdbf7d8a8a6bb
Nikolay Petkov <106302204+napetkov@users.noreply.github.com>
2023-09-23 08:41:50 +0300

    Sample Spring Boot Application

---
+++ Spring_Fundamentals_Sept_2023/boot-demo/src/main/java/org/softuni/bootdemo/web/HelloController.java
@@ -0,0 +1,21 @@
+package org.softuni.bootdemo.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    private final String greeting;
+
+    public HelloController(@Value("${demo.greeting.massage}") String greeting){
+        this.greeting = greeting;
+    }
+
+    @GetMapping("/test")
+    public String test(Model model){
+        model.addAttribute("greeting", greeting);
+        return "test";
+    }
+}
