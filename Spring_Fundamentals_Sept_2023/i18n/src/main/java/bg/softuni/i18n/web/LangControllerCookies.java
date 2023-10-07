56aad3b54e12800e4eaecc7976b162a9f1fe695e
Nikolay Petkov <106302204+napetkov@users.noreply.github.com>
2023-10-07 09:59:42 +0300

    Demo cookies.session

---
+++ Spring_Fundamentals_Sept_2023/i18n/src/main/java/bg/softuni/i18n/web/LangControllerCookies.java
@@ -0,0 +1,35 @@
+package bg.softuni.i18n.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class LangControllerCookies {
+
+    @GetMapping("/cookies")
+    public String langCookies(@CookieValue(
+            value = "lang",
+            defaultValue = "bg")String lang,
+                                Model model){
+        model.addAttribute("lang",lang);
+
+        return "lang";
+    }
+
+    @PostMapping("/cookies")
+    public String langCookies(@RequestParam("lang") String lang, HttpServletResponse response){
+        Cookie cookie = new Cookie("lang",lang);
+
+        response.addCookie(cookie);
+
+
+        return "redirect:/cookies";
+    }
+}
