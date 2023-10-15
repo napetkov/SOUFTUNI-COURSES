c469c4b98231d380e7e096eb80a1223ea7d71ee8
Nikolay Petkov <106302204+napetkov@users.noreply.github.com>
2023-10-15 17:09:24 +0300

    Entity Graph demo.

--- Spring_Fundamentals_Sept_2023/mobilele/src/main/java/org/softuni/mobilele/repository/BrandRepository.java
+++ Spring_Fundamentals_Sept_2023/mobilele/src/main/java/org/softuni/mobilele/repository/BrandRepository.java
@@ -2,13 +2,22 @@ package org.softuni.mobilele.repository;
 
 import org.softuni.mobilele.model.entity.BrandEntity;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface BrandRepository extends JpaRepository<BrandEntity,Long> {
+public interface BrandRepository extends JpaRepository<BrandEntity, Long> {
     @Override
     List<BrandEntity> findAll(Sort sort);
+
+    @EntityGraph(
+            value = "brandWithModels",
+            attributePaths = "models"
+    )
+    @Query("SELECT b FROM BrandEntity b")
+    List<BrandEntity> getAllBrands();
 }
